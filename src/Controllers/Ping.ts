2f9563ea7c7ca9c87986d1a4e7b7761f48947e9e
capriok <capriok7@gmail.com>
2021-11-23 12:00:20 -0700

    Update

--- src/Controllers/Ping.ts
+++ src/Controllers/Ping.ts
@@ -2,7 +2,5 @@ import { Request, Response } from 'express'
 
 export const Ping = async (req: Request, res: Response) => {
 	console.log('Request: Ping!')
-	setTimeout(() => {
-		res.json({ status: 200 })
-	}, 2000)
+	res.json({ status: 200 })
 }
