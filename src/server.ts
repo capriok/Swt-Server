ddd3c69378bb5a8f36aa4a6f018f09c4a5ef3232
capriok <capriok7@gmail.com>
2022-02-28 14:13:08 -0700

    Update: cors origins

--- src/server.ts
+++ src/server.ts
@@ -20,6 +20,7 @@ app.use(express.json())
 const origins = [
 	'http://192.168.0.233:3000',
 	'http://localhost:3000',
+	'https://swt.kylecaprio.dev',
 	'https://sweetie.kylecaprio.dev',
 	'https://api.weatherapi.com'
 ]
