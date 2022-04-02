1e94e781edc5b4fd29363e3cf88c2b016f215073
capriok <capriok7@gmail.com>
2022-04-02 10:40:16 -0700

    Calendar today mapping

--- src/Functions/Calendar.ts
+++ src/Functions/Calendar.ts
@@ -1,68 +1,76 @@
-import { compareAsc, isBefore, isSameDay } from 'date-fns'
-import { ServerDate } from './Index'
-import { TimezoneDate } from './Time'
+import { compareAsc, isBefore, isSameDay } from "date-fns";
+import { ServerDate } from "./Index";
+import { TimezoneDate, TimezoneZero } from "./Time";
 
 interface CalendarDay {
-  number: number
-  events: Array<CalendarEvent>
+  number: number;
+  events: Array<CalendarEvent>;
   classNames: {
-    number: string
-    day: string
-  }
+    number: string;
+    day: string;
+  };
 }
 
 interface CalendarEvent {
-  _id?: string
-  name: string
-  timed: boolean
-  date: string
-  startTime: string
-  endTime: string
+  _id?: string;
+  name: string;
+  timed: boolean;
+  date: string;
+  startTime: string;
+  endTime: string;
 }
 
 export function SortByDate(arr: Array<any>) {
-  return arr.sort((a, b) => compareAsc(new Date(a.date), new Date(b.date)))
+  return arr.sort((a, b) => compareAsc(new Date(a.date), new Date(b.date)));
 }
 
 export function FormatDates(arr: Array<any>) {
   return arr.map((x: any) => {
-    x.date = TimezoneDate(x.date).toJSON()
-    return x
-  })
+    x.date = TimezoneDate(x.date).toJSON();
+    return x;
+  });
 }
 
 export function FilterBeforeCurrentMonth(events) {
   return events.filter((e) => {
-    const eventDate = new Date(e.date).getTime()
-    const lastDayofLastMonth = new Date(new Date().getFullYear(), new Date().getMonth(), 0).getTime()
+    const eventDate = new Date(e.date).getTime();
+    const lastDayofLastMonth = new Date(
+      new Date().getFullYear(),
+      new Date().getMonth(),
+      0
+    ).getTime();
 
-    return eventDate > lastDayofLastMonth && e
-  })
+    return eventDate > lastDayofLastMonth && e;
+  });
 }
 
-export function GenerateCalendar(ces: Array<CalendarEvent>): Array<CalendarDay> {
-  const days: Array<CalendarDay> = new Array()
-  const day = new Date(new Date().getFullYear(), new Date().getMonth(), 1)
-  const weekday = day.getDay()
+export function GenerateCalendar(
+  ces: Array<CalendarEvent>
+): Array<CalendarDay> {
+  const days: Array<CalendarDay> = new Array();
+  const day = new Date(new Date().getFullYear(), new Date().getMonth(), 1);
+  const weekday = day.getDay();
 
-  MapDays(day)
+  MapDays(day);
 
   function MapDays(day: Date, d = 0) {
-    const isToday = isSameDay(day, ServerDate)
-    const inPast = isBefore(day, ServerDate)
-
     d === 0 && weekday === 0
       ? day.setDate(day.getDate() - 7)
       : d === 0
-        ? day.setDate(day.getDate() + (d - weekday))
-        : day.setDate(day.getDate() + 1)
+      ? day.setDate(day.getDate() + (d - weekday))
+      : day.setDate(day.getDate() + 1);
+
+    const isToday = isSameDay(TimezoneZero(day), ServerDate);
+    const inPast = isBefore(TimezoneZero(day), ServerDate);
+
+    console.log(TimezoneZero(day), ServerDate, isToday);
 
     let dayEvents = ces.filter((ce) => {
-      return isSameDay(new Date(ce.date), day)
-    })
+      return isSameDay(new Date(ce.date), day);
+    });
 
-    const numberClassName = `number${isToday ? ' today' : ''}`
-    const dayClassName = `day${inPast && !isToday ? ' blur' : ''}`
+    const numberClassName = `number${isToday ? " today" : ""}`;
+    const dayClassName = `day${inPast && !isToday ? " blur" : ""}`;
 
     let calendarDay: CalendarDay = {
       number: day.getDate(),
@@ -71,11 +79,11 @@ export function GenerateCalendar(ces: Array<CalendarEvent>): Array<CalendarDay> 
         number: numberClassName,
         day: dayClassName,
       },
-    }
-    days.push(calendarDay)
+    };
+    days.push(calendarDay);
 
-    d < 41 && MapDays(day, (d = d + 1))
+    d < 41 && MapDays(day, (d = d + 1));
   }
 
-  return days
+  return days;
 }
